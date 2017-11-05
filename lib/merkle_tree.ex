4cd1fc6cc349d27b33d8f2fe421240c4e62d0305
Lucca <julienlucca@gmail.com>
2017-11-05 04:13:26 -0200

    Add verification and height to MerkleTree

--- lib/merkle_tree.ex
+++ lib/merkle_tree.ex
@@ -10,24 +10,30 @@ defmodule ExCoin.MerkleTree do
   }
 
   def new(blocks) when blocks != [] do
+    unless is_power_of_n(@number_of_children, Enum.count(blocks)) do
+      raise %ArgumentError{message: "must be power of #{@number_of_children}"}
+    end
+
     root = build(blocks)
     %__MODULE__{root: root, blocks: blocks}
   end
 
   def build(blocks) do
-    Enum.map(blocks, &build_block/1)
-    |> build_tree()
+    initial_height = 0
+    Enum.map(blocks, &build_first_block/1)
+    |> build_tree(initial_height)
   end
 
-  def build_block(block) do
+  def build_first_block(block) do
     %Node{
       value: ExCoin.hash(block),
-      children: []
+      children: [],
+      height: 0
     }
   end
 
-  def build_tree([root]), do: root
-  def build_tree(nodes) do
+  def build_tree([root], _), do: root
+  def build_tree(nodes, height) do
     children = Enum.chunk_every(nodes, @number_of_children)
     parents = Enum.map(children, fn (partition) ->
       concatenated =
@@ -38,9 +44,15 @@ defmodule ExCoin.MerkleTree do
       # Parent node hash is calculated by the concat of its children
       %Node{
         value: ExCoin.hash(concatenated),
-        children: partition
+        children: partition,
+        height: height + 1
       }
     end)
-    build_tree(parents)
+    build_tree(parents, height + 1)
+  end
+
+  def is_power_of_n(n, blocks) do
+    (:math.log2(blocks) / :math.log2(n))
+    |> ( fn x -> Float.ceil(x) == Float.floor(x) end ).()
   end
 end
