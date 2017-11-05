0075a9d044998298e2fbe365c24d4c53c2a8ef81
Lucca <julienlucca@gmail.com>
2017-11-05 03:03:59 -0200

    Fix recursion error

--- lib/merkle.ex
+++ lib/merkle.ex
@@ -4,16 +4,18 @@ defmodule ExCoin.Merkle do
   @number_of_children 2
 
   def build(blocks) do
-    leaves = Enum.map(blocks, fn (block) ->
-      %Node{
-        value: ExCoin.hash(block),
-        children: []
-      }
-    end)
-    build_tree(leaves)
+    Enum.map(blocks, &build_block/1)
+    |> build_tree()
   end
 
-  def build_tree([root], _), do: root
+  def build_block(block) do
+    %Node{
+      value: ExCoin.hash(block),
+      children: []
+    }
+  end
+
+  def build_tree([root]), do: root
   def build_tree(nodes) do
     children = Enum.chunk_every(nodes, @number_of_children)
     parents = Enum.map(children, fn (partition) ->
@@ -22,6 +24,7 @@ defmodule ExCoin.Merkle do
         |> Enum.map(&(&1.value))
         |> Enum.reduce("", &(&2 <> &1))
 
+      # Parent node hash is calculated by the concat of its children
       %Node{
         value: ExCoin.hash(concatenated),
         children: partition
--- lib/signer.ex
+++ lib/signer.ex
@@ -1,10 +1,19 @@
 defmodule ExCoin.Signer do
-  def run(message \\ "hello") do
-    curve = :secp256k1
-    {public_key, private_key} = :crypto.generate_key(:ecdh, curve)
+  @curve :secp256k1
+  def sign(message) do
+    {public_key, private_key} = :crypto.generate_key(:ecdh, @curve)
 
-    signature = :crypto.sign(:ecdsa, :sha256, message, [private_key, curve])
-    valid? = :crypto.verify(:ecdsa, :sha256, message, signature, [public_key, curve])
-    valid?
+    signature = :crypto.sign(:ecdsa, :sha256, message, [private_key, @curve])
+    {message, signature, public_key}
+  end
+
+  def is_valid?({ message, signature, public_key }) do
+    :crypt.verify(:ecdsa, :sha256, message, signature, [public_key, @curve])
+  end
+
+  def run(message) do
+    message
+    |> sign()
+    |> is_valid?()
   end
 end
