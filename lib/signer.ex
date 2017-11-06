a9b75c673b6c2054d0dc8dd92b5302e795cec5a6
Lucca <julienlucca@gmail.com>
2017-11-05 22:52:11 -0200

    Improve signer.ex

--- lib/signer.ex
+++ lib/signer.ex
@@ -1,19 +1,38 @@
 defmodule ExCoin.Signer do
-  @curve :secp256k1
-  def sign(message) do
-    {public_key, private_key} = :crypto.generate_key(:ecdh, @curve)
+  @moduledoc """
+  Simple wrapper of the :crypto module.
+  Used mainly for experimentation
+  """
 
-    signature = :crypto.sign(:ecdsa, :sha256, message, [private_key, @curve])
-    {message, signature, public_key}
+  @curve :secp256k1
+
+  @doc """
+  Just sample function for experimentation
+  """
+  def run(message) do
+    {public_key, private_key} = :crypto.generate_key(:ecdh, @curve)
+    run(message, public_key, private_key)
   end
 
-  def is_valid?({message, signature, public_key}) do
+  @doc """
+  Just sample function for experimentation
+  """
+  def run(message, public_key, private_key) do
+    signature = sign(message, private_key)
+
+    if is_valid?(message, signature, public_key) do
+      signature
+    else
+      {:error, :invalid_signature}
+    end
+  end
+
+  def sign(message, private_key) do
+    :crypto.sign(:ecdsa, :sha256, message, [private_key, @curve])
+  end
+
+  def is_valid?(message, signature, public_key) do
     :crypto.verify(:ecdsa, :sha256, message, signature, [public_key, @curve])
   end
 
-  def run(message) do
-    message
-    |> sign()
-    |> is_valid?()
-  end
 end
